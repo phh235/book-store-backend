1a7ee2f28ab5bb173603bcdafb8b95de89326344
phh235 <hoangaatrox23504@gmail.com>
2024-04-23 15:57:34 +0700

    First commit

---
+++ src/test/java/vn/phh235/bookstorebackend/BookStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.phh235.bookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
