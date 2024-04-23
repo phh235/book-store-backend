6968a0ed44fd11446254f0d36475020d43ad3773
phh235 <hoangaatrox23504@gmail.com>
2024-04-24 01:16:45 +0700

    Create entity successfully

--- src/test/java/vn/phh235/bookstorebackend/BookStoreBackendApplicationTests.java
+++ src/test/java/vn/phh235/bookstorebackend/BookStoreBackendApplicationTests.java
@@ -1,13 +1,18 @@
 package vn.phh235.bookstorebackend;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.stubbing.answers.AnswersWithDelay;
 import org.springframework.boot.test.context.SpringBootTest;
+import vn.phh235.bookstorebackend.entity.TheLoai;
 
 @SpringBootTest
 class BookStoreBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		TheLoai theLoai = new TheLoai();
+		theLoai.setMaTheLoai(1);
+		theLoai.setTenTheLoai("Java OOP");
 	}
 
 }
